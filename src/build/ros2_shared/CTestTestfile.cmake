84400de4edb100853a884808e5a143644867d15e
paulfersi <128245752+paulfersi@users.noreply.github.com>
2024-06-28 08:00:47 +0200

    removed useless files

--- src/build/ros2_shared/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sonia/orbslam3-tello-drone/src/ros2_shared
-# Build directory: /home/sonia/orbslam3-tello-drone/src/build/ros2_shared
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/build/tello_msgs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sonia/orbslam3-tello-drone/src/tello_msgs
-# Build directory: /home/sonia/orbslam3-tello-drone/src/build/tello_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tello_msgs__py")
