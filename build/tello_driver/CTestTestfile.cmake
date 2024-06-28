3ecf34f809c3a47f4850b6168f6f4e91a44689d1
paulfersi <128245752+paulfersi@users.noreply.github.com>
2024-06-28 08:25:02 +0200

    Delete build directory

--- build/tello_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sonia/orbslam3-tello-drone/src/tello_driver
-# Build directory: /home/sonia/orbslam3-tello-drone/build/tello_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tello_driver/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/sonia/orbslam3-tello-drone/install/ros2_shared/share/ros2_shared/package.sh"
-. "/home/sonia/orbslam3-tello-drone/install/tello_msgs/share/tello_msgs/package.sh"
