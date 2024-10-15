5ba276528a9a1e707f4a5e1df9a5eae6c06a6334
paulfersi <paolfersi@gmail.com>
2024-10-15 18:45:08 +0200

    modified launch file

--- src/drone_info_gui/launch/drone_info_gui_launch.py
+++ src/drone_info_gui/launch/drone_info_gui_launch.py
@@ -1,12 +1,27 @@
+from ament_index_python import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='drone_info_gui',  # Replace with your package name
-            executable='drone_info_gui_node',  
-            name='drone_info_gui_node',
-            output='screen',
-        )
-    ])
+
+
+    waypoint_node = Node(
+        name='drone_info_gui',
+        package='drone_info_gui',
+        executable='drone_info_gui_node',
+        output="screen"
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                'use_sim_time',
+                default_value='false',
+                description='Use simulation (Gazebo) clock if true'
+            ),
+            waypoint_node
+        ]
+    )
