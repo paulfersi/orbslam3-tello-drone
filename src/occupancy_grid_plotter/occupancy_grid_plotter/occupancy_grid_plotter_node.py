23b6792c75c932af3b4c54eedee85a8e1362f039
paulfersi <paolfersi@gmail.com>
2025-04-09 09:24:46 +0200

    modify grid plotting with separate threading

--- src/occupancy_grid_plotter/occupancy_grid_plotter/occupancy_grid_plotter_node.py
+++ src/occupancy_grid_plotter/occupancy_grid_plotter/occupancy_grid_plotter_node.py
@@ -1,41 +1,56 @@
-from nav_msgs.msg import OccupancyGrid
 import rclpy
-from rclpy.node import Node 
+from rclpy.node import Node
+
+from nav_msgs.msg import OccupancyGrid
 import numpy as np
 import matplotlib.pyplot as plt
+import threading
 
 class OccupancyGridPlotter(Node):
     def __init__(self):
         super().__init__('occupancy_grid_plotter')
-        
-        self.occupancy_grid_sub = self.create_subscription(OccupancyGrid,'/projected_map',self.callback,10)
+        self.subscription = self.create_subscription(
+            OccupancyGrid,
+            '/projected_map',
+            self.listener_callback,
+            10
+        )
+        self.lock = threading.Lock()
+        self.map_data = None
 
-    def callback(self,msg):
-        self.plot_occupancy_grid(msg)
+        # Start a separate thread for plotting
+        threading.Thread(target=self.plot_loop, daemon=True).start()
 
-    def plot_occupancy_grid(self,occupancy_grid):
-        width = occupancy_grid.info.width
-        height = occupancy_grid.info.height
-        data = np.array(occupancy_grid.data,dtype=np.int8)
+    def listener_callback(self, msg: OccupancyGrid):
+        self.get_logger().info(f'Received map: {msg.info.width} x {msg.info.height}')
+        data = np.array(msg.data).reshape((msg.info.height, msg.info.width))
+        with self.lock:
+            self.map_data = data
 
-        grid = data.reshape((height,width))
+    def plot_loop(self):
+        plt.ion()
+        fig, ax = plt.subplots()
+        img = None
 
-        grid = np.where(grid==.1,50,grid)
-
-        plt.figure(figsize=(8,8))
-        plt.imshow(grid,cmap='gray',origin='lower')
-        plt.title("Occupancy Grid")
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.show()
-
-        
-
+        while rclpy.ok():
+            with self.lock:
+                if self.map_data is not None:
+                    if img is None:
+                        img = ax.imshow(self.map_data, cmap='gray', origin='lower')
+                    else:
+                        img.set_data(self.map_data)
+                    ax.set_title('Occupancy Grid Map')
+                    plt.draw()
+                    plt.pause(0.1)
 
 def main(args=None):
     rclpy.init(args=args)
     node = OccupancyGridPlotter()
-    rclpy.spin(node)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print("Shutting down")
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
